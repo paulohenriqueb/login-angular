95665386eb166ca921d076415326350baff70bf2
Paulo Henrique B da Silva <paulohenriqueb.silva@gmail.com>
2022-09-14 22:27:03 -0300

    Criado o Service principal de ambas interfaces Person/Product

--- src/app/main/main.service.ts
+++ src/app/main/main.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { catchError, Observable, throwError, tap } from 'rxjs';
+import { Person } from './person';
+import { Product } from './product';
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
+  private readonly base_url: string= 'http://localhost:9000/api';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPeople():Observable<Person[]>{
+    return this.http.get<Person[]>(`${this.base_url}/person`)
+      .pipe(
+        tap(p=>console.log(p)),
+        catchError((e)=>{
+          console.log(e)
+          return throwError(e);
+        })
+      )
+  }
+
+  getProduct():Observable<Product[]>{
+    return this.http.get<Product[]>(`${this.base_url}/product`)
+      .pipe(
+        tap(pr=>console.log(pr)),
+        catchError((e)=>{
+          console.log(e);
+          return throwError(e);
+        })
+      )
+  }
+
+
 }
