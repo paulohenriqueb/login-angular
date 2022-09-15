4da2461d6482eae9ff346ad725e09cafecc94ebb
Paulo Henrique B da Silva <paulohenriqueb.silva@gmail.com>
2022-09-15 10:45:35 -0300

    Criado as rotas principais de listagem de produtos e pessoas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/main/people' },
+  { path: 'main', loadChildren:()=> import('./main/main.module').then( r=> r.MainModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
