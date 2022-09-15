f0286607c335535d3484932bbc8b130176dcab8e
Paulo Henrique B da Silva <paulohenriqueb.silva@gmail.com>
2022-09-14 22:52:30 -0300

    Criado o observable que puxa as informações do banco de dados para a listagem de produtos

--- src/app/main/products/products.component.ts
+++ src/app/main/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from '../main.service';
+import { Observable } from 'rxjs';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  products$?: Observable<Product[]>
+  constructor(private mainService: MainService) { }
 
   ngOnInit(): void {
+    this.products$ = this.mainService.getProduct();
   }
 
 }
